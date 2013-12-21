f177267fb5e0a141209cfcd1d93dcc5e34531b96
Sumedh Mungee <smungee@gmail.com>
2013-12-20 19:10:10 -0800

    simple function works

--- src/ev3clj/core.clj
+++ src/ev3clj/core.clj
@@ -4,27 +4,71 @@
 ; unless youre running on an EV3
 (defn getMotorUsingReflection [motor]
   (let [clazz (Class/forName "lejos.nxt.Motor")]
-    (.getField class motor)))
+    (.getField clazz motor)))
 
-(defn left [] (getMotorUsingReflection "B"))
-(defn right [] (getMotorUsingReflection "C"))  
+(def left lejos.nxt.Motor/B)
+(def right lejos.nxt.Motor/C)  
 
-(defn setSpeed [motor speed]
-  (println (str "Set speed to " speed))
-  (.setSpeed motor speed))
+(defn setSpeed
+  ([motor speed]
+     (println (str "Set speed to " speed))
+     (.setSpeed motor speed))
+  ([speed]
+     (.setSpeed left speed)
+     (.setSpeed right speed)))
 
-(defn startmotor [motor]
+(defn forward! [motor]
   (.forward motor))
 
-(defn stopmotor [motor]
+(defn backward! [motor]
+  (.backward motor))
+
+(defn stopmotor! [motor]
   (.stop motor))
 
-(defn forward [speed]
+(defn forward
+  ([speed]
+     (setSpeed left speed)
+     (setSpeed right speed)
+     (forward! left)
+     (forward! right))
+  ([left_speed right_speed]
+     (setSpeed left left_speed)
+     (setSpeed right right_speed)
+     (forward! left)
+     (forward! right)))
+ 
+
+(defn backward [speed]
 ;  (map #(setSpeed %1 speed) [left right])
 ;  (map #(setSpeed %1 speed) [left right])
   (setSpeed left speed)
   (setSpeed right speed)
-  (map startmotor [left right]))
+  (backward! left)
+  (backward! right))
 
 (defn stop []
-  (map stopmotor [left right]))
+  (stopmotor! left)
+  (stopmotor! right))
+             
+
+(defn wait [millisec]
+  (Thread/sleep (* millisec 1000)))
+
+(defn turn [angle]
+  (do
+    (.rotate left (* angle (/ 900 90)))))
+
+(defn goBack [speed]
+ (forward speed)
+ (wait 10)
+ (stop)
+ (turn 180)
+ (forward speed)
+ (wait 10)
+ (stop))
+ 
+
+ 
+ 
+ 
